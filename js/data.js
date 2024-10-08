fd442c49af0157a650457e7e727838c6143d4872
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2024-10-08 02:38:36 +0000

    Make CSS for mobile devices first

--- js/data.js
+++ js/data.js
@@ -1,2 +1,16 @@
 "use strict";
-/* exported data */
+const dataKey = 'movie-search-data';
+const data = readData();
+function readData() {
+    const localData = localStorage.getItem(dataKey);
+    if (localData) {
+        return JSON.parse(localData);
+    }
+    return {
+        view: 'search-form',
+        movies: [],
+    };
+}
+function writeData() {
+    localStorage.setItem(dataKey, JSON.stringify(data));
+}
